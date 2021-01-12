4f04ea63ebbfe50e4b1d1b56ef68972161c5b8b5
Florian Paul Schmidt <mista.tapas@gmx.net>
2021-01-12 22:43:27 +0100

    incorporate robin gareus' fixes

--- tanh.cc
+++ tanh.cc
@@ -1,31 +1,31 @@
 #include <cmath>
 #include <lv2.h>
 
-inline float db_to_gain( float x) 
+static inline float db_to_gain( float x) 
 {
-    return pow(10, x/20.0);
+    return powf(10.0f, 0.05f * x);
 }
 
 struct clipping_tanh {
     float *ports[6];
 };
 
-LV2_Handle instantiate(const LV2_Descriptor *descriptor, double sample_rate, const char *bundle_path, const LV2_Feature *const *features)
+static LV2_Handle instantiate(const LV2_Descriptor *descriptor, double sample_rate, const char *bundle_path, const LV2_Feature *const *features)
 {
     return (LV2_Handle)(new clipping_tanh);
 }
 
-void cleanup(LV2_Handle instance)
+static void cleanup(LV2_Handle instance)
 {
     delete ((clipping_tanh*)instance);
 }
 
-void connect_port(LV2_Handle instance, uint32_t port, void *data_location)
+static void connect_port(LV2_Handle instance, uint32_t port, void *data_location)
 {
     ((clipping_tanh*)instance)->ports[port] = (float*)data_location;
 }
 
-void run(LV2_Handle instance, uint32_t sample_count)
+static void run(LV2_Handle instance, uint32_t sample_count)
 {
     clipping_tanh *tinstance = (clipping_tanh*)(instance);
 
@@ -41,11 +41,11 @@ void run(LV2_Handle instance, uint32_t sample_count)
         const float in2 = pregain * (in + bias);
         
         tinstance->ports[1][sample_index] = dry * in +
-            wet * postgain * tanh(in2);
+            wet * postgain * tanhf(in2);
     }
 }
 
-LV2_Descriptor descriptor = {
+static const LV2_Descriptor descriptor = {
     "http://fps.io/plugins/clipping.tanh",
     instantiate,
     connect_port,
