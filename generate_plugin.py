68449b0545e3cda2411acb8917372f726dffd2ea
FPS <mista.tapas@gmx.net>
2021-01-14 11:56:32 +0100

    lalala

--- generate_plugin.py
+++ generate_plugin.py
@@ -2,13 +2,14 @@ from optparse import OptionParser
 import json
 
 option_parser = OptionParser()
-option_parser.add_option("-i", "--input", dest="input", help="A JSON file containing a plugin description", default="plugins.json")
-option_parser.add_option("-x", "--oversampling-factor", dest="oversample", help="The oversampling factor used", default="1")
-option_parser.add_option("-c", "--channels", dest="channels", help="The audio input/output channels count", default="1")
-option_parser.add_option("-n", "--name", dest="name", help="The plugin name", default="name")
-option_parser.add_option("-v", "--version", dest="version", help="The plugin version", default="1")
-option_parser.add_option("-o", "--output", dest="output", help="The output file base name", default="output")
-option_parser.add_option("-d", "--description", dest="description", help="The description", default="A plugin")
+option_parser.add_option("-i", "--input", dest="input", help="A JSON file containing a plugin description. Default: %default", default="plugins.json")
+option_parser.add_option("-x", "--oversampling-factor", dest="oversample", help="The oversampling factor used. Default: %default", default="1")
+option_parser.add_option("-c", "--channels", dest="channels", help="The audio input/output channels count. Default: %default", default="1")
+option_parser.add_option("-s", "--smooth-control-parameters-cutoff", help="The normalized radians/sec cutoff frequency for the control parameter smoothing lowpass. Default: %default", default="0.1")
+option_parser.add_option("-n", "--name", dest="name", help="The plugin name. Default: %default", default="name")
+option_parser.add_option("-v", "--version", dest="version", help="The plugin version. Default: %default", default="1")
+option_parser.add_option("-o", "--output", dest="output", help="The output file base name. Default: %default", default="output.${name}.c${channels}.o${oversample}.v${version}")
+option_parser.add_option("-d", "--description", dest="description", help="The description. Default: %default", default="A plugin")
 
 (options, args) = option_parser.parse_args()
 
@@ -17,18 +18,21 @@ def replace_options_in_string(s, options):
 
 with open(options.input) as input_file:
   input_json = json.load(input_file)
-  with open(options.output + ".ttl", "w") as ttl_output_file, open(options.output + ".manifest.ttl", "w") as manifest_output_file:
+  ttl_output_file_name = replace_options_in_string(options.output, options) + ".ttl"
+  manifest_output_file_name = replace_options_in_string(options.output, options) + ".manifest.ttl" 
+  with open(ttl_output_file_name, "w") as ttl_output_file, open(manifest_output_file_name, "w") as manifest_output_file:
     ttl_output_file.write("@prefix lv2:  <http://lv2plug.in/ns/lv2core#> .\n")
     ttl_output_file.write("@prefix foaf: <http://xmlns.com/foaf/0.1/> .\n")
     ttl_output_file.write("@prefix doap: <http://usefulinc.com/ns/doap#> .\n")
     ttl_output_file.write("\n")
     uri = replace_options_in_string(input_json["uri"], options)
     ttl_output_file.write(f"<{uri}> a lv2:Plugin ;\n")
+    manifest_output_file.write(f"<{uri}> a lv2:Plugin ; rdfs:seeAlso <{ttl_output_file_name}> .\n")
     name = replace_options_in_string(input_json["name"], options)
     ttl_output_file.write(f"  doap:name \"{name}\" ;\n")
     ttl_output_file.write("  doap:license <http://usefulinc.com/doap/licenses/gpl> ;\n")
     ttl_output_file.write(f"  doap:description \"{options.description}\" ;\n")
-    ttl_output_file.write(f"  lv2:binary <{name}.so>\n")
+    ttl_output_file.write(f"  lv2:binary <{name}.so> ;\n")
     ttl_output_file.write("  lv2:optionalFeature lv2:hardRTCapable ;\n")
 
 
